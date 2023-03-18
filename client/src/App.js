b2fc276f9afb6e28a39c2250a8637c21b7ebf224
GeorgiStoyanov05 <g.stoyanov05@gmail.com>
2023-03-18 14:03:30 +0200

    Deleted everything connected with ContactUs and Client pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import {Routes, Route} from 'react-router-dom'
 import Index from './components/Index/Index';
 import About from './components/About/About'
-import Client from './components/Client/Client';
-import Contact from './components/Contact/Contact';
 import Books from './components/Games/Books';
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
       <Routes>
         <Route  path='/' element = {<Index />}/>
         <Route path='/about' element = {<About />} />
-        <Route  path='/client' element = {<Client/>} />
-        <Route path='/contacts' element = {<Contact />}/>
         <Route path='/games' element = {<Books />}/>
       </Routes>
     </div>
