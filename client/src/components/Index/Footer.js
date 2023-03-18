6a58c44419c1b6f142ce4a3e6203fc3941cc1976
GeorgiStoyanov05 <g.stoyanov05@gmail.com>
2023-03-18 13:48:23 +0200

    Removed the "Subscribe" field

--- client/src/components/Index/Footer.js
+++ client/src/components/Index/Footer.js
@@ -2,20 +2,6 @@ export default function Footer(){
     return (
         <div className="footer_section layout_padding">
   <div className="container">
-    <div className="input-group mb-3">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Enter your email"
-        aria-label="Enter your email"
-        aria-describedby="basic-addon2"
-        />
-      <div className="input-group-append">
-        <span className="input-group-text" id="basic-addon2">
-          Subscribe
-        </span>
-      </div>
-    </div>
     <div className="social_icon">
       <ul>
         <li>
