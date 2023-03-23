8c46cd896a2dcb6d010a680f59bfa22e9536ebd0
GeorgiStoyanov05 <g.stoyanov05@gmail.com>
2023-03-23 11:49:12 +0200

    Make the picture work without positioning

--- client/src/components/Songs/Song.js
+++ client/src/components/Songs/Song.js
@@ -1,18 +1,17 @@
-export default function Song(
+export default function Song({
     name,
     singer,
     genre,
     imageUrl,
     description
-){
+}){
     return (
         <div className="col-lg-3 col-sm-6">
-            <div className="image_2">
+            <div className="image_0" style={{backgroundImage: `url(${imageUrl})`}}>
               <h1 className="number_text">01</h1>
-              <h1 className="game_text_1">Game</h1>
+              <h1 className="game_text_1">{name}</h1>
               <p className="many_text">
-                There are many variations of passages of Lorem Ipsum available,
-                but the{" "}
+                {singer}
               </p>
             </div>
             <div className="playnow_bt active">
